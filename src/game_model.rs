a82b784076ea5e07eb8276c74f109c484f87afc7
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-02-04 22:01:34 +0000

    Pushing to github to get a handy from @currysoup :3

--- src/game_model.rs
+++ src/game_model.rs
@@ -21,56 +21,44 @@ pub enum ArenaCell {
 // A cell locaiton in the arena
 #[derive(Clone, Copy)]
 pub struct CellLocation {
-    x: i32,
-    y: i32
+    x: usize,
+    y: usize
 }
 
 pub struct Arena {
         cells: Vec<Vec<ArenaCell>>,
         snakeHead : CellLocation,
         snakeTail : CellLocation,
-        length: i32
+        length: usize
 }
 
-fn get_arena_centre(arena_length: i32) -> CellLocation {
+fn get_arena_centre(arena_length: usize) -> CellLocation {
     let mid = arena_length / 2;
     CellLocation { x: mid, y: mid }
 }
 
 fn add_food_random_cell(arena: &mut Vec<Vec<ArenaCell>>) {
-    let mut rng = thread_rng();
-
-    // Halp... I don't know what i'm doing. I should probably read a rust tutorial or two
-    // before i try to use it :P. Screw it, I'm going to bed...
-
-    let flattenedCells = arena.iter().flatMap(|x| x);
-
-    let emptyCells = flattenedCells.iter().filter(|cell| match cell {
-        Empty => true,
-        _ => false
-    }).collect::<Vec<ArenaCell>>();
-
-    let cell = rng.choose_mut(emptyCells);
-
-    match cell {
-        Some(c) => *c = ArenaCell::Food,
-        None => println!("fdgdfgsdf")
-    }
 
 }
 
-fn create_empty_cell_grid(length: i32) -> Vec<Vec<ArenaCell>> {
+fn create_empty_cell_grid(length: usize) -> Vec<Vec<ArenaCell>> {
     (0 .. length).map(|_|
         (0 .. length).map(|_| ArenaCell::Empty ).collect()
     ).collect()
 }
 
 impl Arena {
-    fn new(length: i32) -> Arena {
+    fn new(length: usize) -> Arena {
         let mut cells = create_empty_cell_grid(length);
         let startLocation = get_arena_centre(length);
 
-        add_food_random_cell(&mut cells);
+        let CellLocation { x, y } = startLocation;
+
+        if let Some(startCell) = cells.get_mut(x).and_then(|row| row.get_mut(y)) {
+            startCell = &mut ArenaCell::SnakePart { direction:Direction::Right }
+        }
+
+        //add_food_random_cell(&cells);
 
         Arena { cells : cells,
                 snakeHead: startLocation,
